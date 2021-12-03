2d1c3ceb3d80c81dba23d82d5f6f1b46cb1485d6
leah16c <chen.lmw@gmail.com>
2021-12-03 10:03:00 +0000

    Testing API

--- main.js
+++ main.js
@@ -2,4 +2,10 @@ function handleRecipeClick() {}
 
 function handleInputChange() {}
 
-async function fetchRecipe(food) {}
+async function fetchRecipe(food) {
+
+    let response = await fetch("https://api.edamam.com/api/recipes/v2?type=public&q=cheese&app_id=54bc5425&app_key=7b4349e0a80c3457542551922ada680");
+    let data = await reSVGTextPositioningElement.json();
+
+    console.log(data);
+}
