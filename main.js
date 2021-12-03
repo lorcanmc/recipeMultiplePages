53a0d75b764d60a8ecd74cbfc3baaac3c37156d9
leah16c <chen.lmw@gmail.com>
2021-12-03 15:17:05 +0000

    Styling Update.

--- main.js
+++ main.js
@@ -2,13 +2,15 @@
 let searchInput = document.querySelector("#food-input");
 let searchButton = document.querySelector("#recipe-button");
 
+//handling searchButton click
 function handleRecipeClick() {
     let searchString = searchInput.value;
     fetchRecipe(searchString);
 }
-searchButton.addEventListener("click", handleRecipeClick);             //searchButton event listener
+searchButton.addEventListener("click", handleRecipeClick);
 
 
+//adds ingredient to the bullet point list in #recipe-info
 function addIngredientToList(ingredient) {
     let newListItem = document.createElement("li");
     newListItem.innerText = ingredient.text;
@@ -16,12 +18,12 @@ function addIngredientToList(ingredient) {
     list.appendChild(newListItem);
 }
 
-
+//fetches data from API and updates webpage
 async function fetchRecipe(searchString) {
 
+    //fetches data
     let response = await fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=${searchString}&app_id=54bc5425&app_key=d7b4349e0a80c3457542551922ada680`);
     let data = await response.json();
-    console.log(data)
 
     //chooses a recipe object and stores it in variable called recipe
     let randomIndex = Math.floor(Math.random()*data.hits.length)
@@ -30,64 +32,35 @@ async function fetchRecipe(searchString) {
 
     //upper background image
     let backgroundImageURL = recipe.images.REGULAR.url;
-    console.log(backgroundImageURL);
     let backgroundImageDiv = document.querySelector("#show-recipe");
-    console.log(backgroundImageDiv);
     backgroundImageDiv.style.background = `url(${backgroundImageURL})`;
 
 
     //meal name
     let recipeNameString = recipe.label;
-    console.log(recipeNameString);
     let recipeName = document.querySelector("#recipe-name");
-    console.log(recipeName);
     recipeName.innerText = recipeNameString;        
     
     //calories
     let caloriesNumber = Math.round(recipe.calories);
-    console.log(caloriesNumber);
     let calories = document.querySelector("#calories");
-    console.log(calories);
     calories.innerText = `${caloriesNumber} calories`;
-
-    // //thumbnail image
-    // let recipeThumbnailURL = recipe.images.THUMBNAIL.url;
-    // let recipeThumbnail = document.querySelector("#recipe-thumbnail");
-    // recipeThumbnail.src = recipeThumbnailURL;
     
     //ingredients list
     let recipeIngredients = recipe.ingredients;
     let prevIngredLis = document.querySelectorAll("#ingredients-list li");
     for(let i=0; i<prevIngredLis.length; i++) {
         prevIngredLis[i].remove();
-    }                                    //deletes previous ingredient list
+    }                                                   //deletes previous ingredient list
     recipeIngredients.forEach(addIngredientToList);     //adds the new ingredients list
 
-    // OPTIONS FOR LABELS:
+    // OPTIONS FOR MORE LABELS:
     //cuisineType
     //dishType
     //mealType
     //healthLabel
-    //dietLabel    //dishType
-    //mealType
-    //healthLabel
     //dietLabel
-
 }
 
 
-fetchRecipe("tofu");
-
-
-
-
-
-
-// let cardContainer = document.querySelector("#card-container")
-// function createrecipeCard() {
-//     let recipeCard = document.createElement("div");
-//     //add class
-//     recipeCard.classList.add("cardStyle");
-//     //append to #card-container
-//     cardContainer.appendChild("recipeCard");
-// }
+fetchRecipe("tofu");                //called when webpage loads
